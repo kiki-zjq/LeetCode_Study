5920fc26f994dd798e73cdc79320d9302db1a53e
kiki-zjq <582133739@qq.com>
2020-09-14 22:01:29 +0800

    feat:Add 09/14

---
+++ Arith/JavaScript/94.二叉树的中序遍历.js
@@ -0,0 +1,41 @@
+/** 
+ * 2020/09/14
+ *
+ *  
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ * 
+ * */
+
+var inorderTraversal = function(root) {
+    const res = [];
+    const inorder = (root) => {
+        if (!root) {
+            return;
+        }
+        inorder(root.left);
+        res.push(root.val);
+        inorder(root.right);
+    }
+    inorder(root);
+    return res;
+};
